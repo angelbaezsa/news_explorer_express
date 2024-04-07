1356cba6fff2a08d90e17ff68a7ddd8e0fa76f07
Angel Baez <angelbaezsa@gmail.com>
2024-04-07 14:23:00 -0400

    added necessary files, created database

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/news_explorer_db", (r) =>
+  console.log(`DataBase error ${r}`)
+);
 
 app.listen(PORT, () => console.log(`app listening in port: ${PORT}`));
